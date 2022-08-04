accfa76a0cb4dd7faa9fb2652369a129639e5048
Victor Lance <victor_lance@hotmail.com>
2022-08-04 20:57:17 -0300

    Estudo sobre Properties e Value

---
+++ src/main/java/com/springpropertiesvalue/SpringPropertiesValueApplication.java
@@ -0,0 +1,17 @@
+package com.springpropertiesvalue;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringPropertiesValueApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPropertiesValueApplication.class, args);
+    }
+}
+
